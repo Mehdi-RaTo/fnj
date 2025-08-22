b15dba1f89804684966d6a67799d4c020abf8326
Mehdi-RaTo <Mehdi.RaTo@Gmail.Com>
2025-08-22 22:42:50 +0330

    ...

--- src/fnj.js
+++ src/fnj.js
@@ -1,5 +1,15 @@
 class fnj { }
 
+// Alias
+
+window.fnj = fnj;
+window.convert = fnj;
+
+// Functions
+
+/**
+ * Show Developer
+ */
 fnj.developer = function () {
     console.log("Mehdi-RaTo");
 }
