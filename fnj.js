8a01362043246028834cedbe8e7a24fab082ef74
Mehdi-RaTo <Mehdi.RaTo@Gmail.Com>
2025-08-22 23:47:34 +0330

    ...

---
+++ fnj.js
@@ -0,0 +1,14 @@
+class fnj { }
+
+// ========== Alias
+window.fnj = fnj;
+window.convert = fnj;
+
+// ========== Functions
+
+/**
+ * Show Developers
+ */
+fnj.developer = function () {
+    console.info("Mehdi-RaTo", "HKH");
+}
